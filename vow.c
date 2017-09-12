558dba8c2e696f0d3011b846a9aca3546da3c53b
gayathricse <31653477+gayathricse@users.noreply.github.com>
2017-09-12 16:32:58 +0530

    Create vow.c

---
+++ vow.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char{'a','b','i','0',n};
+printf("enter the integers");
+scanf("%d",&integers);
+if(n==1)
+{
+printf("the integer is vowel");
+else
+{
+printf("the integer is not vowel");
+}
+}
+                                                                                                                                                      
