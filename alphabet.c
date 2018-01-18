6130aba17914aa6021a8c938632217c0716b00c3
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-01-18 16:56:47 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h.
+int main()
+{
+int a,b,c;
+printf("enter the values");
+scanf("%d",&a,&b,&c);
+if(c=a,e,i,o,u)
+{
+printf("the value is alphabet");
+}
+else
+{
+printf("the value is not alphabet");
+}
+}
