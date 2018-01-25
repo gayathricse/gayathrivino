39585e9fec6af2007386e0ab20af93085eb6687a
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-01-25 16:36:59 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,9 @@
+#include<math.h>
+void  main()
+{
+int a,d,n,i,tn;
+int sum=0;
+printf("enter the first term value of A.P series:");
+scanf("%d",&a);
+printf("enter the total number of value A.P series:");
+}
