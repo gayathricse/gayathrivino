e144c74bcf71d3497171d1742bd83dea44141d3b
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-01 07:58:33 +0000

    Create d4.c

---
+++ d4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char s[30];
+int i,flag=0,n=0;
+clrscr();
+printf("enter the string");
+scanf("%ss",s[30]);
+for(i=0;s[i]!='\0';i++)
+{
+issf(s[i]=='0'\\s[i]=='1'\\s[i]=='2'\\s[i]=='3'\\s[i]=='4'\\s[i]=='5'\\s[i]=='6'\\s[i]=='7'\\s[i]=='8'\\s[i]=='9')
+{
+flag=flag+1;
+}
+}
+if(flag==0)
+{
+printf("string not contain number");
+}
+else
+{
+printf("string contains number");
+}
+return 0;
+}
