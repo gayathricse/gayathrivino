bbdc7ea1013e398e729cb82e17b2778e6eca1680
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-01 07:54:20 +0000

    Create d2.c

---
+++ d2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int h,m,total mins;
+{
+printf("enter the times in hours");
+printf("hours");
+printf("mins");
+scanf("%d,&m);
+total mintues=(h+60)+m;
+printf("enter the mintues");
+}
+return 0;
+}
