04e21e7bfadc66889410653983fc99b174132506
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-01 07:56:10 +0000

    Create d3.c

---
+++ d3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n,a[20],i;
+printf("enter the number");
+printf("Enter the integers");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+printf("index is%d\t%d\n",a[i],i);
+return 0;
+}
