70e71bf4a6b8e8d39c746c271328c085a4e17f97
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-06 10:14:12 +0000

    Create string.c

---
+++ string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,n=4;
+char a[7];
+printf("enter the string");
+scanf("%s",&a);
+for(i=1;i<=n;i++)
+{
+printf("%s \n",&a);
+}
+return 0;
+}
