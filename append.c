cc52d2b603bffec66cf0c6426fb8fac094962255
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-06 11:04:28 +0000

    Create append.c

---
+++ append.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[200];
+int i,j;
+gets(a);
+j=strlen(a);
+for(i=0;i<=j;i++)
+{
+if(i==j)
+    {
+    printf(".");}
+    else
+{
+printf("%c",a[i]);}
+}
+return 0;
+}
