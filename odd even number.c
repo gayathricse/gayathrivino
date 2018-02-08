3075af9f809de47fd4baafaa37cd6464c111fd90
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-08 11:02:10 +0000

    Create odd even number.c

---
+++ odd even number.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+void main()
+{
+	int a,b,product;
+	scanf("%d%d",&a,&b);
+	product=a*b;
+	if(product%2==0)
+{
+printf("the number is even"\n);
+}
+else
+{
+	printf("the number is odd"\n);
+}
+
