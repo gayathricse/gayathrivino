5873e4691b3622e6da9008514c4f6ae34b23ec53
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-08 11:06:12 +0000

    Create alphabet number.c

---
+++ alphabet number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char str;
+printf("Enter the string:");
+scanf("%c",&str);
+printf("Is the string containing alphabets and numbers:");
+if(str>='A' && str<='Z' && str>='0' && str<='9' || str>='a' && str<='z' && str>='0' && str<='9')
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+
