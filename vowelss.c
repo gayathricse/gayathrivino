3cadaf672ada62ab7938db1ce2893fef1cb56f75
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-13 16:03:58 +0530

    Create vowelss.c

---
+++ vowelss.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the a");
+scanf("%d",&a);
+if((a=='a')||(a=='A')||(a=='e')||(a=='E')||(a=='i')||(a=='I')||(a=='o')||(a=='O')||(a=='u')||(a=='U'))
+{
+printf("vowel is present");
+}
+else
+{
+printf("vowel is not present");
+}
+return 0;
+}
