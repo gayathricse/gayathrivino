b0555b6df4b0eafc2d3e5c7426a71c7acfa87c5f
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-02-13 16:24:07 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char a[20];
+printf("enter the value");
+scanf("%s",&a);
+if((a=="monday")||(a=="MONDAY")||(a=="tuesday")||(a=="TUESDAY")||(a=="wednesday")||(a=="WEDNESDAY")||(a=="thursday")||(a=="THURSDAY")||(a=="friday")||(a=="FRIDAY"))
+{
+printf("NO");
+}
+if((a=="sunday")||(a=="SUNDAY")||(a=="saturday")||(a=="SATURDAY")||)
+{
+printf("YES");
+}
+}
