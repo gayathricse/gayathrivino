70873493db27cf70b7c72cb0206f3a7bb5546825
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-03-01 16:22:03 +0530

    Create mm.c

---
+++ mm.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("the number is multiple of 13");
+}
+else
+{
+printf("the number is not multiple of 13");
+}
+return 0;
+}
