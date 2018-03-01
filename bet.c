ef586b7d6144dd23b717bdc7538f458b17ea1254
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-03-01 15:56:29 +0530

    Create bet.c

---
+++ bet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int n,a,b;
+printf("enter n:");
+scanf("%d",&n);
+printf("/n a and b");
+scanf("%d%d",&a,&b);
+if(a<n<b)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
