52e4591fcac6998e959b6a8979dba3382031c5ff
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-03-08 16:38:44 +0530

    Create cel.c

---
+++ cel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float cel,kelvin;
+printf("enter the celsius value");
+scanf("%f",&cel);
+kelvin==(cel+273);
+printf("kelvin value %f:",cel+273);
+return 0;
+}
