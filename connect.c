cb992b15cd23ca2c3bd357fb1789a8769357b6b4
gayathricse <31653477+gayathricse@users.noreply.github.com>
2018-03-20 16:10:18 +0530

    Create connect.c

---
+++ connect.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int anumber,bnumber;
+    printf("enter the numbers");
+    scanf("%d%d",&anumber, &bnumber);
+    printf("%d%d",anumber,bnumber);
+    return 0;
+}
